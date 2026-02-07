d38a3663817003b2579542090aa18b11d010ea21
Firas Mosbehi <firas.mosbehi@insat.ucar.tn>
2026-02-07 19:19:48 +0100

    test: mock asset manifest for google_fonts in widget tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,11 +5,29 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter_test/flutter_test.dart';
+import 'dart:convert';
+import 'dart:typed_data';
 
 import 'package:devtools_pro/main.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
+  final binding = TestWidgetsFlutterBinding.ensureInitialized();
+  binding.defaultBinaryMessenger.setMockMessageHandler(
+    'flutter/assets',
+    (message) async {
+      final key = utf8.decode(message!.buffer.asUint8List());
+      if (key == 'AssetManifest.json') {
+        final data = utf8.encode('{}');
+        return ByteData.view(Uint8List.fromList(data).buffer);
+      }
+      return ByteData(0);
+    },
+  );
+  GoogleFonts.config.allowRuntimeFetching = false;
+
   testWidgets('Home lists utilities', (WidgetTester tester) async {
     await tester.pumpWidget(const DevToolsProApp());
 
